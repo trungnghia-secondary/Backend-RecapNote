e40a2cca0e251f01fb82b924c852241bf5ec8183
trungnghia-secondary <jyfuyfuutyuyoufuguy@gmail.com>
2025-09-13 11:47:38 +0700

    Update process_job.py

--- processing_service/process_job.py
+++ processing_service/process_job.py
@@ -4,7 +4,9 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 
 shared_path = os.path.join(parent_dir, 'shared')
-
+if shared_path not in sys.path:
+    sys.path.insert(0, shared_path)
+    
 import tempfile
 import time
 import traceback
