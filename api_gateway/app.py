d444db73382bde4b3409a7292c1f5aff8d640995
trungnghia-secondary <jyfuyfuutyuyoufuguy@gmail.com>
2025-09-13 20:43:26 +0700

    Update app.py

--- api_gateway/app.py
+++ api_gateway/app.py
@@ -1,12 +1,13 @@
 # api_gateway/app.py
 import os
 import sys
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
 
-shared_path = os.path.join(parent_dir, '/app/shared')
-if shared_path not in sys.path:
-    sys.path.insert(0, shared_path)
+#current_dir = os.path.dirname(os.path.abspath(__file__))
+#parent_dir = os.path.dirname(current_dir)
+
+#shared_path = os.path.join(parent_dir, '/app/shared')
+#if shared_path not in sys.path:
+#    sys.path.insert(0, shared_path)
     
 import uuid
 import time
