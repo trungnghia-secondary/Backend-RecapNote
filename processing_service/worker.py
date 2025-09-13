250a6a211ecf2ba04071f0e5dc48d3130fb0e48b
trungnghia-secondary <jyfuyfuutyuyoufuguy@gmail.com>
2025-09-13 21:37:34 +0700

    Update worker.py

--- processing_service/worker.py
+++ processing_service/worker.py
@@ -3,16 +3,16 @@ import sys
 import os
 
 # Lấy đường dẫn tuyệt đối của thư mục cha (ví dụ: 'processing_service')
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
+#current_dir = os.path.dirname(os.path.abspath(__file__))
+#parent_dir = os.path.dirname(current_dir)
 
 # Thêm thư mục 'shared' vào sys.path
-shared_path = os.path.join(parent_dir, '/app/shared')
-if shared_path not in sys.path:
-    sys.path.insert(0, shared_path)
+#shared_path = os.path.join(parent_dir, '/app/shared')
+#if shared_path not in sys.path:
+#    sys.path.insert(0, shared_path)
     
 import time
-from shared.db import SessionLocal, init_db, Job
+from db import SessionLocal, init_db, Job
 from process_job import process_job
 from dotenv import load_dotenv
 load_dotenv()
