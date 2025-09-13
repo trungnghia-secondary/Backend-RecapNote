73feb6db0dcee774ff93a7214526be4554eecf96
trungnghia-secondary <jyfuyfuutyuyoufuguy@gmail.com>
2025-09-13 18:22:59 +0700

    Update worker.py

--- processing_service/worker.py
+++ processing_service/worker.py
@@ -1,5 +1,16 @@
 # processing_service/worker.py
+import sys
 import os
+
+# Lấy đường dẫn tuyệt đối của thư mục cha (ví dụ: 'processing_service')
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+# Thêm thư mục 'shared' vào sys.path
+shared_path = os.path.join(parent_dir, 'Backend-RecapNote/shared')
+if shared_path not in sys.path:
+    sys.path.insert(0, shared_path)
+    
 import time
 from shared.db import SessionLocal, init_db, Job
 from process_job import process_job
